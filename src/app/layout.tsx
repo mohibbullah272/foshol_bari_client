3d7d6018294ed8220b421ab8143709f44d0e3e49
mohibbullah272 <bullahmohi50@gmail.com>
2025-11-06 17:08:03 +0600

    add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import AuthProvider from "@/provider/AuthProvider";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -55,6 +55,7 @@ export default function RootLayout({
         <AuthProvider>
         {children}
         </AuthProvider>
+        <SpeedInsights/>
         <Toaster></Toaster>
       
       </body>
